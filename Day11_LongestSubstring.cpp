1dfd11b266c665b5b0e6225247fcc367220e1d92
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-10-16 11:39:49 +0530

    Update Day11_LongestSubstring.cpp

--- Day11_LongestSubstring.cpp
+++ Day11_LongestSubstring.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n = s.length();
-        int maxs = INT_MIN;
-        for(int i =1; i<n;i++){
-            if(s[i]==s[i-1]){
-                count=0;
+        int i=0,j=0,count=0,maxi=0;
+        vector<char> vec;
+        vector<char>::iterator it;
+        while(i<s.size()){
+            while(j<s.size()){
+                it=find(vec.begin(),vec.end(),s[j]);
+                if(it!=vec.end()){
+                    count=vec.size();
+                    maxi=max(maxi,count);
+                    vec.clear();
+                    i++;
+                    break;
+                }
+                else{
+                    vec.push_back(s[j]);
+                    j++;
+                }
             }
-            else{
-                count++;                    
-            }
-
-            maxs = max(maxs,count);
-        }        
+            j=i;
+        }
+        return maxi;
     }
-};      
+};
