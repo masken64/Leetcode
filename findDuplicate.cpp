be6036677edf4b0bec25bd25212dc2f3c79f41b7
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-10-21 13:40:00 +0530

    Create findDuplicate.cpp

---
+++ findDuplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int> set;
+        for(int i =0;i<n;i++){
+            if(set.count(nums[i])){
+                return nums[i];
+            }
+            set.insert(nums[i]);
+        }
+        return -1;
+        
+    }
+};
