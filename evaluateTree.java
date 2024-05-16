6ae461d826532ebb1853bb4c645e24c699d77bea
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2024-05-16 14:34:41 +0530

    Create evaluateTree.java

---
+++ evaluateTree.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        if(root.val == 0) return false;
+        if(root.val == 1) return true;
+
+        boolean left = evaluateTree(root.left);
+        boolean right = evaluateTree(root.right);
+
+        return root.val == 2 ? left|right : left&right;
+    }
+}
